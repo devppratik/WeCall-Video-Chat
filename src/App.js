d411c1bd7fc27fd89161a465840f74116403426e
devppratik <pratikpanda24@gmail.com>
2022-05-24 10:31:21 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import Video from './Video'
 import Home from './Home'
+import CheckScreen from "./CheckScreen";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
 		return (
-			<div>
-				<Router>
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/:url" component={Video} />
-					</Switch>
-				</Router>
-			</div>
-		)
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/check/:url" component={CheckScreen} />
+            <Route path="/:url" component={Video} />
+          </Switch>
+        </Router>
+      </div>
+    );
 }
 
 export default App;
